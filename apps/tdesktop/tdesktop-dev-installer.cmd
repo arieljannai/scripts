63850dc4ed7300bf032bd235780e9faa085f8801
Ariel Jannai <arieljannai@gmail.com>
2018-02-04 20:55:45 +0200

    WIP

--- apps/tdesktop/tdesktop-dev-installer.cmd
+++ apps/tdesktop/tdesktop-dev-installer.cmd
@@ -3,7 +3,7 @@
 setlocal EnableDelayedExpansion
 set bad=false
 
-:: Verify needed commands exist
+REM Verify needed commands exist
 where git >nul 2>&1
 IF %errorlevel% NEQ 0 (
 	@echo **git** was not found.
@@ -48,12 +48,13 @@ IF NOT EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Comm
 )
 
 IF "%bad%" == "false" (
-	rem call :check-admin
+	call :check-admin
 	IF !errorlevel! NEQ 0 @echo. && @echo. && goto usage
 	@echo All the dependencies are met.
 	@echo You can start the script^^!
 	@echo.
 	pause
+	@echo.
 ) ELSE (
 	@echo.
 	@echo If you had any problems or missing programs, 
@@ -85,10 +86,7 @@ IF %errorlevel% NEQ 0 (
 	curl --silent -L -O https://raw.githubusercontent.com/chocolatey/choco/fdfcd06/src/chocolatey.resources/redirects/RefreshEnv.cmd
 )
 
-goto :eof
-:: remove this line ^^^ to activate
-
-:: All good, start the actual script.
+REM All good, start the actual script.
 set TD_ROOT_DIR=
 
 IF "%1"=="/?" goto usage
@@ -112,16 +110,16 @@ IF "%TD_ROOT_DIR%"=="" (
 mkdir %TD_ROOT_DIR%\Libraries 2> NUL
 mkdir %TD_ROOT_DIR%\ThirdParty 2> NUL
 
-IF ERRORLEVEL 1 (
-	@echo The path "%TD_ROOT_DIR%" is not empty
-	goto script-end
-)
+rem IF ERRORLEVEL 1 (
+rem 	@echo The path "%TD_ROOT_DIR%" is not empty
+rem 	goto script-end
+rem )
 
 set LIBS=%TD_ROOT_DIR%\Libraries
 set THIRD_PARTY=%TD_ROOT_DIR%\ThirdParty
 
-:: TODO: Check if curl, grep, head, tail, 7z exist and if not --> download them
 
+pushd %THIRD_PARTY%
 call :get-download-links
 call :download-installation-files
 call :script-start
@@ -132,9 +130,8 @@ call :remove-uneeded-files
 goto script-end
 
 
-:: ### MAIN ###
+REM ### MAIN ###
 :script-start
-pushd %THIRD_PARTY%
 for %%a in (*.*) do (
 	set tmp=%%a
 	set strt=!tmp:~0,3!
@@ -156,90 +153,105 @@ for %%a in (*.*) do (
 		call :install-yasm !tmp!
 	)
 )
-call :install-gyp
+rem call :install-gyp
 exit /b
-:: ### END OF MAIN ###
+REM ### END OF MAIN ###
 
 
-:: ### GET DOWLOAD LINKS ###
+REM ### GET DOWLOAD LINKS ###
 :get-download-links
-@rem perl
+@REM perl
 curl --silent https://www.activestate.com/activeperl/downloads 2>&1 | grep -m1 -oP "(http://downloads.*?MSWin32-x64.*?\.exe)"  | head --lines=1 > tmpfile
 set /p downlink= < tmpfile
 echo %downlink% > installation-links.txt
 
-@rem nasm
+@REM nasm
 curl --silent http://www.nasm.us/pub/nasm/releasebuilds/?C=M;O=D;F=0 2>&1 | grep -m1 -oP "((?:\d+\.?){3}/)" | head --lines=1 > tmpfile
 set /p downlink= < tmpfile
 set downlink=%downlink:~0,-1%
 set downlink=http://www.nasm.us/pub/nasm/releasebuilds/%downlink%/win64/nasm-%downlink%-win64.zip
 echo %downlink% >> installation-links.txt
 
-@rem yasm
+@REM yasm
 curl --silent http://yasm.tortall.net/Download.html 2>&1 | grep -m1 -oP "http(.*win64\.exe)" | head --lines=1 > tmpfile
 set /p downlink= < tmpfile
 echo %downlink% >> installation-links.txt
 
-@rem msys2
+@REM msys2
 curl --silent http://repo.msys2.org/distrib/x86_64/ 2>&1 | grep -oP "msys2.*?tar\.xz" | tail --lines=1 > tmpfile
 set /p downlink= < tmpfile
 set downlink=http://repo.msys2.org/distrib/x86_64/%downlink%
 echo %downlink% >> installation-links.txt
 
-@rem jom
+@REM jom
 set downlink=http://download.qt.io/official_releases/jom/jom.zip
 echo %downlink% >> installation-links.txt
 
-@rem python 2.7
+@REM python 2.7
 curl --silent https://www.python.org/downloads/ 2>&1 | grep -oP "https.*python/2.*\.msi" | head --lines=1 > tmpfile
 set /p downlink= < tmpfile
 echo %downlink% >> installation-links.txt
 
-@rem cmake
+@REM cmake
 curl --silent https://cmake.org/download/ 2>&1 | grep -m1 -oP "/files/.*/cmake.*win64.*\.zip.>" | head --lines=1 > tmpfile
 set /p downlink= < tmpfile
 set downlink=%downlink:~0,-2%
 set downlink=https://cmake.org%downlink%
 echo %downlink% >> installation-links.txt
 
-@rem ninja
+@REM ninja
 set downlink=https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-win.zip
-echo %downlink% >> installation-links.txt
+echo|set /p=%downlink% >> installation-links.txt
 
 del tmpfile
 set downlink=
 exit /b
-:: ### END OF GET DOWLOAD LINKS ###
+REM ### END OF GET DOWLOAD LINKS ###
 
 
-:: ### DOWNLOAD INSTALLATION FILES ###
+REM ### DOWNLOAD INSTALLATION FILES ###
 :download-installation-files
 @echo == Downloading third party installation files: ==
+for /F "tokens=*" %%a in (installation-links.txt) do call :download-http-file %%a
+exit /b
 
-for /F "tokens=*" %%l in (installation-links.txt) do (
-	IF NOT "%%l" == "" (
-		@echo %%l
-		curl -L -O --progress-bar %%l
-		@echo.
+rem :get-files-for-removal
+rem for %a in (*) do @echo %a >> removal.txt
+rem 
+rem exit /b
+REM ### END OF DOWNLOAD INSTALLATION FILES ###
+
+REM ### DOWNLOAD HTTP FILE ###
+:download-http-file
+@echo %~1
+set dlURL=%~1
+for %%x in (%~1) do set currFileName=%%~nxx
+curl -L -O --progress-bar %~1
+7z t %currFileName% 2>&1 | grep ERROR >nul
+IF !errorlevel! == 0 (
+	@echo Download %currFileName% failed. Trying one more time.
+	curl -L -O --progress-bar !dlURL!
+	7z t %currFileName% 2>&1 | grep ERROR >nul
+	IF !errorlevel! == 0 (
+		@echo Download failed again. Please manually download !dlURL!
+		@echo And install it according to the relevant instructions at:
+		@echo https://github.com/telegramdesktop/tdesktop/blob/dev/docs/building-msvc.md
+		goto :eof
 	)
 )
-
-:get-files-for-removal
-for %a in (*) do @echo %a >> removal.txt
-
 exit /b
-:: ### END OF DOWNLOAD INSTALLATION FILES ###
+REM ### END OF DOWNLOAD HTTP FILE ###
 
 
-:: ### REMOVE UNEEDED FILES ###
+REM ### REMOVE UNEEDED FILES ###
 :remove-uneeded-files
 pushd %THIRD_PARTY%
 for /f %%f in ('dir /b . /A-D') do del /Q %%f
 popd
 exit /b
-:: ### END OF REMOVE UNEEDED FILES ###
+REM ### END OF REMOVE UNEEDED FILES ###
 
-:: ### THIRD PARTY INSTALL FUNCTIONS ###
+REM ### THIRD PARTY INSTALL FUNCTIONS ###
 :install-perl
 @echo Installing perl
 %1 /qb APPDIR="%THIRD_PARTY%\Perl"
@@ -297,10 +309,10 @@ exit /b
 git clone https://chromium.googlesource.com/external/gyp
 git -C gyp checkout a478c1ab51
 exit /b
-:: ### END OF THIRD PARTY INSTALL FUNCTIONS ###
+REM ### END OF THIRD PARTY INSTALL FUNCTIONS ###
 
 
-:: ### EXTRA FUNCTIONS ###
+REM ### EXTRA FUNCTIONS ###
 :check-admin
 net session >nul 2>&1
 if not %errorlevel% == 0 (
@@ -308,17 +320,18 @@ if not %errorlevel% == 0 (
 	echo Please start the script in an elevated command line.
 )
 exit /b
-:: ### END OF EXTRA FUNCTIONS ###
+REM ### END OF EXTRA FUNCTIONS ###
 
-:: ### ADD RELEVANT THIRD PARTY TO PATH ###
+REM ### ADD RELEVANT THIRD PARTY TO PATH ###
 :update-path
 cmd
 RefreshEnv.cmd
 setx /M PATH "%PATH%;%THIRD_PARTY%\gyp;%THIRD_PARTY%\Ninja;
+exit
 exit /b
-:: ### END OF ADD RELEVANT THIRD PARTY TO PATH ###
+REM ### END OF ADD RELEVANT THIRD PARTY TO PATH ###
 
-:: ### VERIFY THIRD PARTY FOLDERS EXIST ###
+REM ### VERIFY THIRD PARTY FOLDERS EXIST ###
 :verify-third-party-folders
 IF NOT EXIST "%THIRD_PARTY%\Perl" call :notify-missing-third-party-folder "%THIRD_PARTY%\Perl"
 IF NOT EXIST "%THIRD_PARTY%\NASM" call :notify-missing-third-party-folder "%THIRD_PARTY%\Perl"
@@ -328,20 +341,21 @@ IF NOT EXIST "%THIRD_PARTY%\jom" call :notify-missing-third-party-folder "%THIRD
 IF NOT EXIST "%THIRD_PARTY%\Python27" call :notify-missing-third-party-folder "%THIRD_PARTY%\Perl"
 IF NOT EXIST "%THIRD_PARTY%\cmake" call :notify-missing-third-party-folder "%THIRD_PARTY%\Perl"
 IF NOT EXIST "%THIRD_PARTY%\Ninja" call :notify-missing-third-party-folder "%THIRD_PARTY%\Perl"
-:: ### END OF VERIFY THIRD PARTY FOLDERS EXIST ###
+exit /b
+REM ### END OF VERIFY THIRD PARTY FOLDERS EXIST ###
 
-:: ### THIRD PARTY FOLDER IS MISSING ###
+REM ### THIRD PARTY FOLDER IS MISSING ###
 :notify-missing-third-party-folder
 @echo The folder %~1 is missing. Keeping all installation files - please fix it manually. Not continuing to install Libraries.
 exit /b
-:: ### END OF THIRD PARTY FOLDER IS MISSING ###
+REM ### END OF THIRD PARTY FOLDER IS MISSING ###
 
-:: ### USAGE ###
+REM ### USAGE ###
 :usage
 @echo Usage: tdesktop-dev-installer.bat ^<tdesktop-buildpath^>
-@echo Note: Remember to run the script with elevated permissions.
+@echo Note: REMember to run the script with elevated permissions.
 exit /b 1
-:: ### END OF USAGE ###
+REM ### END OF USAGE ###
 
 :script-end
 @echo Done^^!
