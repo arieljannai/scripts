ebdcde801b77b00851a89b48b411bddc1814bd27
Ariel Jannai <arieljannai@gmail.com>
2020-10-09 04:44:19 +0300

    cleaning the code

--- maintenance/update-readme.py
+++ maintenance/update-readme.py
@@ -3,21 +3,24 @@
 import sys
 import os
 import stat
-# import subprocess as sp
-# import re
 
 HOME_DIR = '.'
 DIR = '├'
 SUBDIR = '│'
 LAST_DIR = '└'
 INDENT = '─'
-# D, S, L, C = '    '
-# SP = ' '
+
+# ascii version
+# DIR = '+'
+# SUBDIR = '|'
+# LAST_DIR = '\'
+# INDENT = '-'
+
 IGNORE_LIST = ['.idea', 'maintenance']
 
 
 def list_files(startpath, level, is_last, prefix):
-	level += 1
+	treeString = ''
 	objects = os.listdir(startpath)
 	dirs = [name for name in objects if os.path.isdir(os.path.join(startpath, name))]
 	files = [name for name in objects if os.path.isfile(os.path.join(startpath, name))]
@@ -27,15 +30,21 @@ def list_files(startpath, level, is_last, prefix):
 
 	for i in range(files_count):
 		curr_name = files[i]
-		prefix_a = (prefix + (SUBDIR + '   ') * bool(level - 1) * bool(dirs_count))
+		b_level, b_dirs = bool(level), bool(dirs_count)
+		
+		prefix_a = prefix + (SUBDIR + '   ') * b_level * b_dirs
 		prefix_b = ('    ' * is_last) + \
-					((SUBDIR + '   ') * (not is_last) * (bool(level - 1))) + \
-				 	('    ' * (is_last or bool(level - 1) and (not bool(dirs_count))))
-		print('{}{}'.format(prefix_a + prefix_b, curr_name))
+					((SUBDIR + '   ') * (not is_last) * b_level) + \
+				 	('    ' * b_level * (not b_dirs))
+
+		output = '{}{}{}'.format(prefix_a, prefix_b, curr_name)
+		treeString += output + '\n'
+		# print(output)
 
 		if ((i + 1) == files_count):
-			print(prefix_a + prefix_b)
-
+			output = prefix_a + prefix_b
+			treeString += output + '\n'
+			# print(output)
 
 	for i in range(dirs_count):
 		curr_name = dirs[i]
@@ -45,24 +54,22 @@ def list_files(startpath, level, is_last, prefix):
 
 		curr_obj_path = os.path.join(startpath, curr_name)
 		if os.path.isdir(curr_obj_path):
-			prefix_a = prefix + '    ' if is_last else (SUBDIR + '   ') * (level - 1)
+			prefix_a = prefix + '    ' if is_last else (SUBDIR + '   ') * level
 			prefix_b = (LAST_DIR if (i + 1) == dirs_count else DIR) + INDENT*3
-			print('{}{}'.format(prefix_a + prefix_b, curr_name))
+
+			output = '{}{}{}'.format(prefix_a, prefix_b, curr_name)
+			treeString += output + '\n'
+			# print(output)
 			
-			list_files(curr_obj_path, level, (i + 1) == dirs_count, prefix_a)
+			treeString += list_files(curr_obj_path, level + 1, (i + 1) == dirs_count, prefix_a)
 
 			if (is_last):
-				print(prefix)
-			
+				output = prefix
+				treeString += output + '\n'
+				# print(output)
 
-# def tree_dir(dir, tab_result=False):
-# 	output = sp.run(['C:\\Windows\\System32\\tree.com', '/a', '/f', dir], stdout=sp.PIPE).stdout.decode('utf-8')
-# 	match = re.findall(r'Volume.*?\r\r.*?\r\r(.*?)$', output.replace('\n', '\r'), flags=re.MULTILINE)[0]
-# 	tree_result = match.replace('\r\r', '\r\n')
-# 	if tab_result:
-# 		return re.sub(r'^', '\t', tree_result, flags=re.MULTILINE)
-# 	else:
-# 		return tree_result
+	return treeString
+			
 
 def is_hidden(file):
 	return bool(os.stat(file).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
@@ -72,24 +79,15 @@ def ignore_object(name):
 	return is_hidden(name[0]) or (name[1] in IGNORE_LIST) or (name[1].endswith('.git'))
 
 
-# def set_print_mode(fancy, indent_num):
-# 	if (fancy):
-# 		D, S, L, C = DIR_LINE, SUBDIR_LINE, LAST_DIR_LINE, CHAR_INDENT
-# 	else:
-# 		D, S, L, C = '+', '|', '\\', '-'
-	
-# 	n = indent_num - 1
-# 	D, S, L, C, SP = D*n, S*n, L*n, C*n, SP*indent_num
-
-
 def print_usage():
 	usage = 'Usage: update_readme.py [readme-template.md]\n\n readme-template\tSupply one or use default'
 	print(usage)
 
 
 def main():
-	list_files(HOME_DIR, 0, False, (SUBDIR + '   '))
-	# print(tree_dir(HOME_DIR))
+	tree = list_files(HOME_DIR, 0, False, (SUBDIR + '   '))
+	tree = '.\n{}\n{}'.format(SUBDIR, tree)
+	print(tree)
 	pass
 
 
